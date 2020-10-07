8fb15338386c51b9eb0496dc4d8130e9a4ec69a6
Joshua A. Seyer <jaseyer@co.douglas.or.us>
2020-10-07 13:06:03 -0700

    initial commit

---
+++ Desktop-Shortcut-quarantine-deploy1.ps1
@@ -0,0 +1,40 @@
+#################################
+# Quarantine Icon Deployment script 1 v0.1
+# 10/05/2020 Josh Seyer
+# Douglas County, OR
+################################
+
+# Register log event source for Windows event log
+New-EventLog -LogName 'Quarantine-Shortcut-Deploy' -Source 'DC_PS_Script'
+
+# Set target for Shortcut
+$TargetFile = "https://fortimail.co.douglas.or.us/"
+
+# Set location for shortcut.  Public Desktop makes it available for all users
+$ShortcutFile = "$env:Public\Desktop\Quarantine_Email.lnk"
+
+# Create the shortcut
+$WScriptShell = New-Object -ComObject WScript.Shell
+# icon location
+$iconlocation = "\\dcit\software\Scripts\Quarantine-Fortimail\envelopebang.ico"
+$iconfile = "iconfile=" + $iconlocation
+$Shortcut = $WScriptShell.CreateShortcut($ShortcutFile)
+$Shortcut.IconLocation = $iconlocation # icon index 0
+$Shortcut.TargetPath = $TargetFile
+$Shortcut.Save()
+
+# Set Read Only
+Set-ItemProperty -Path $ShortcutFile -Name IsReadOnly -Value $true
+
+# Log Outcome
+if (Test-Path $ShortcutFile) 
+{
+    Write-EventLog -LogName 'Quarantine-Shortcut-Deploy' -Source 'DC_PS_Script' -EntryType 'Information' -EventID 1 -Message 'Quarantine shortcut creation attempt completed'    
+}
+else 
+{
+    Write-EventLog -LogName 'Quarantine-Shortcut-Deploy' -Source 'DC_PS_Script' -EntryType 'Information' -EventID 1 -Message 'Quarantine shortcut creation attempt failed, no shortcut created'    
+}
+
+# Remove log event source for Windows event log
+Remove-EventLog -LogName 'Quarantine-Shortcut-Deploy'
